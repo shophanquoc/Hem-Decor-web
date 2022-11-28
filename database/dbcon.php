94db4b276b1df9da4e170f1277a2ff9e92df807f
NVL <63655212+LAP200I@users.noreply.github.com>
2022-11-28 21:04:02 +0700

    Update dbcon.php

--- database/dbcon.php
+++ database/dbcon.php
@@ -1,7 +1,7 @@
 <?php
 $sname= "localhost";
 $uname= "root";
-$password = "Ngocyen*2102";
+$password = "";
 $db_name = "hemdecor";
 
 $con = mysqli_connect($sname, $uname, $password, $db_name) or die ("cannot connect");
